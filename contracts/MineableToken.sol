1d406762a39d33d84b1b93335ceeef5c916bf493
amexperts <am.experts.online@gmail.com>
2018-06-12 23:09:46 +0200

    more tests added

--- contracts/MineableToken.sol
+++ contracts/MineableToken.sol
@@ -24,4 +24,13 @@ contract MineableToken is Pausable, StandardToken {
     symbol = _symbol;
     decimals = _decimals;
   }
+
+  /**
+   * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender when not paused.
+   * @param _spender The address which will spend the funds.
+   * @param _value The amount of tokens to be spent.
+   */
+  function approve(address _spender, uint256 _value) whenNotPaused public returns (bool) {
+    return super.approve(_spender, _value);
+  }
 }
--- test/Main.js
+++ test/Main.js
@@ -1,11 +1,17 @@
-const { should, EVMThrow, EVMInvalid } = require('./helpers');
+const {
+  should,
+  EVMThrow,
+  EVMInvalid
+} = require('./helpers');
 
 const promisify = (inner) => new Promise((resolve, reject) => inner((err, res) => err ? reject(err) : resolve(res)));
 const getBalance = (addr) => promisify((cb) => web3.eth.getBalance(addr, cb))
 const getTransaction = (txHash) => promisify((cb) => web3.eth.getTransaction(txHash, cb))
 
 const computeCost = async (receipt) => {
-  let { gasPrice } = await getTransaction(receipt.transactionHash);
+  let {
+    gasPrice
+  } = await getTransaction(receipt.transactionHash);
   return gasPrice.times(receipt.gasUsed);
 }
 
@@ -32,12 +38,33 @@ contract('Token Tests', function(accounts) {
    */
   before(async function() {
 
-      // deploy contract with parameters for testing
-      contract = await C.new(_name, _symbol, _decimals);
+    // deploy contract with parameters for testing
+    contract = await C.new(_name, _symbol, _decimals);
   });
 
-  //Base contract tests
-  it("should have an address", async function () {
+  /**
+   * Runs before each test
+   */
+  beforeEach(async function() {
+
+    //ensure contract is unpaused
+    let isPaused = await contract.paused();
+    if (isPaused) {
+      await contract.unpause({
+        from: accounts[0]
+      });
+    }
+
+    //reset contract owner
+    let currentOwner = await contract.owner();
+    await contract.transferOwnership(owner, {
+      from: currentOwner
+    });
+
+  });
+
+  //Base Contract Tests
+  it("should have an address", async function() {
     web3.isAddress(contract.address).should.be.true;
   });
 
@@ -58,6 +85,218 @@ contract('Token Tests', function(accounts) {
     (await contract.owner()).should.equal(owner);
   });
 
-  
+  it("should not be paused on contract creation when Pausable", async function() {
+    (await contract.paused()).should.be.false;
+  });
+
+  //Ownership Tests
+  it("should allow transfer of ownership by owner when Ownable", async function() {
+
+    let newOwner = accounts[9];
+
+    await contract.transferOwnership(newOwner, {
+      from: owner
+    });
+    (await contract.owner()).should.equal(newOwner);
+  });
+
+  it("should not allow transfer of ownership by non-owner when Ownable", async function() {
+
+    let newOwner = accounts[8];
+
+    await contract.transferOwnership(newOwner, {
+      from: accounts[7]
+    }).should.be.rejectedWith(EVMThrow);
+  });
+
+  //Pausable Tests
+  it("should be able to be paused and unpaused by owner when Pausable", async function() {
+
+    await contract.pause({
+      from: owner
+    });
+    (await contract.paused()).should.be.true;
+    await contract.unpause({from: owner});
+    (await contract.paused()).should.be.false;
+  });
+
+  it("should not allow pause by non-owner when Pausable", async function() {
+
+    await contract.pause({
+      from: accounts[1]
+    }).should.be.rejectedWith(EVMThrow);
+  });
+
+  it("should not allow unpause by non-owner if paused when Pausable", async function() {
+
+    await contract.pause({
+      from: owner
+    });
+    (await contract.paused()).should.be.true;
+    await contract.unpause({
+      from: accounts[1]
+    }).should.be.rejectedWith(EVMThrow);
+  });
+
+  //Token Function Tests
+  it("should allow approve() and allowance() when unpaused", async function() {
+
+    (await contract.paused()).should.be.false;
+
+    let xferAmt = 1000;
+
+    let { logs } = await contract.approve(accounts[1], xferAmt, {from: accounts[0]});
+    let { event, args } = logs[0];
+    event.should.equal('Approval');
+    args.should.deep.equal({
+      owner: accounts[0],
+      spender: accounts[1],
+      value: web3.toBigNumber(xferAmt)
+    });
+
+    let allowance = await contract.allowance(accounts[0], accounts[1]);
+    allowance.should.deep.equal(web3.toBigNumber(xferAmt));
+
+    await contract.approve(accounts[1], 0, {from: accounts[0]}); //reset
+  });
+
+  it("should not allow approve() when paused", async function() {
+
+    await contract.pause({from: owner});
+    (await contract.paused()).should.be.true;
+
+    await (contract.approve(accounts[1], 0, {
+      from: accounts[0]}))
+      .should.be.rejectedWith(EVMThrow);
+  });
+
+  it("should not allow transfer() when paused", async function() {
+
+    await contract.pause({from: owner});
+    (await contract.paused()).should.be.true;
+
+    let xferAmt = 1000;
+
+    await contract.transfer(
+      accounts[1],
+      xferAmt,
+      { from: accounts[0] })
+      .should.be.rejectedWith(EVMThrow);
+  });
+
+  it("should not allow transferFrom() when paused", async function() {
+
+    let xferAmt = 1000;
+
+    // need to approve some tokens first
+    let { logs } = await contract.approve(accounts[1], xferAmt, {from: accounts[0]});
+    let { event, args } = logs[0];
+    event.should.equal('Approval');
+
+    //pause the contract
+    await contract.pause({from: accounts[0]});
+
+    //check that transferFrom fails.
+    await contract.transferFrom(
+      accounts[0],
+      null,
+      xferAmt,
+      { from: accounts[1] }).should.be.rejectedWith(EVMThrow);
+  });
+
+  it("should not allow transfer() when _to is null", async function() {
+
+    (await contract.paused()).should.be.false;
+    let xferAmt = 1000;
+
+    await contract.transfer(
+      null,
+      xferAmt,
+      { from: accounts[0] })
+      .should.be.rejectedWith(EVMThrow);
+  });
+
+  it("should not allow transfer() when _to is 0x0000000000000000000000000000000000000000", async function() {
+
+    (await contract.paused()).should.be.false;
+    let xferAmt = 1000;
+
+    await contract.transfer(
+      '0x0000000000000000000000000000000000000000',
+      xferAmt,
+      { from: accounts[0] })
+      .should.be.rejectedWith(EVMThrow);
+  });
+
+  it("should not allow transferFrom() when _to is null", async function() {
+
+    (await contract.paused()).should.be.false;
+    let xferAmt = 1000;
+
+    let { logs } = await contract.approve(accounts[1], xferAmt, {from: accounts[0]});
+    let { event, args } = logs[0];
+    event.should.equal('Approval');
+
+    await contract.transferFrom(
+      accounts[0],
+      null,
+      xferAmt,
+      { from: accounts[1] }).should.be.rejectedWith(EVMThrow);
+
+    await contract.approve(accounts[1], 0, {from: accounts[0]}); //reset
+  });
+
+  it("should not allow transferFrom() when _to is 0x0000000000000000000000000000000000000000", async function() {
+
+    (await contract.paused()).should.be.false;
+    let xferAmt = 1000;
+
+    let { logs } = await contract.approve(accounts[1], xferAmt, {from: accounts[0]});
+    let { event, args } = logs[0];
+    event.should.equal('Approval');
+
+
+    await contract.transferFrom(
+      accounts[0],
+      '0x0000000000000000000000000000000000000000',
+      xferAmt,
+      { from: accounts[1] }).should.be.rejectedWith(EVMThrow);
+
+    await contract.approve(accounts[1], 0, {from: accounts[0]}); //reset
+  });
+
+  it("should allow transferFrom() when properly approved and unpaused", async function() {
+    throw new Error("test not written yet")
+  });
+
+  it("should allow transfer() of Tokens by address owner when unpaused", async function() {
+    throw new Error("test not written yet")
+  });
+
+  //Gas Limit Functions
+  it("should allow setting of gas price by owner when unpaused", async function() {
+    throw new Error("test not written yet")
+  });
+
+  it("should not allow setting of gas price by owner when paused", async function() {
+    throw new Error("test not written yet")
+  });
+
+  it("should only allow setting of gas price by owner", async function() {
+    throw new Error("test not written yet")
+  });
+
+  it("should not allow setting of gas price to 0", async function() {
+    throw new Error("test not written yet")
+  });
+
+  //Mineable Functions
+  it("should reject mining solution if txn gas price is higher than gas price limit", async function() {
+    throw new Error("test not written yet")
+  });
+
+  it("should accept mining solution if txn gas price is lower than gas price limit", async function() {
+    throw new Error("test not written yet")
+  });
 
 });
