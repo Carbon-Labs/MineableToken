df7b857daa90a3aa87510d2927b1ec808917d7dd
amexperts <am.experts.online@gmail.com>
2018-06-14 18:05:06 +0200

    coverage results

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -3,12 +3,11 @@ var MineableToken = artifacts.require("./MineableToken.sol");
 module.exports = function(deployer, network, accounts) {
   if (network === 'development') {
 
-    const [owner, miner] = accounts;
     const _name = "TOKEN NAME";
     const _symbol = "SYM";
     const _decimals = 18;
 
-    deployer.deploy(UTO, owner, "TOKEN NAME", "SYM"); // <-- dummy params
+    deployer.deploy(MineableToken, "TOKEN NAME", "SYM", 18); // <-- dummy params
   } else if (network === 'test') {
     // Do not deploy contracts, each test should deploy them by itself
     console.log(accounts[0]);
--- truffle.js
+++ truffle.js
@@ -17,7 +17,7 @@ module.exports = {
       host: "localhost",
       network_id: "*",
       port: 8555,         // <-- If you change this, also set the port option in .solcover.js.
-      gas: 0xfffffffffff, // <-- Use this high gas value
-      gasPrice: 0x01      // <-- Use this low gas price
+      //gas: 0xfffffffffff, // <-- Use this high gas value
+      gasPrice: 5000000000      // <-- Use this low gas price
     },
 };
